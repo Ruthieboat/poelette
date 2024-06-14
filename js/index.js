dbf7b5a5bb1a845a619016db2a679199d483b1da
Ruth Boateng <ruthboatengs1@gmail.com>
2024-06-14 13:11:43 +0000

    submission changes

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('button')
+btn.onclick = function() {
+    alert('Select a poem tile and make a payment.')
+}
+const buy = document.getElementById('dot')
+buy.onclick = function() {
+    alert('Poem palette.')
+}
+const some = document.getElementById('submit')
+some.onclick = function() {
+    alert('Check you email.')
+}
